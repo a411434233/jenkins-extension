2865b1cdc20446497e5ac7b5553add684e166831
chengJiaWei <411434233@qq.com>
2021-10-23 09:48:29 +0800

    xs-plugins

---
+++ xs-plugins/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 1,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
