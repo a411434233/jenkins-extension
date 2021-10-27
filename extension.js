e73a84fe75aaad1c3040273a1c612f6d4a618cc1
chengJiaWei <411434233@qq.com>
2021-10-27 09:31:23 +0800

    xs-plugins

--- extension.js
+++ extension.js
@@ -3,6 +3,8 @@
 const vscode = require('vscode')
 const fs = require('fs')
 const path = require('path')
+const { copy } = require('./utils')
+const { Uri } = require('vscode')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,51 +22,57 @@ function activate(context) {
   // The commandId parameter must match the command field in package.json
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('pushFiles', function (e) {
-      let urls = e.path.replace('/', '')
-      // fs.appendFile(urls, 'ddd', function (err, data) {
-      //   console.log(err)
-      //   console.log(data)
-      // })
-      fs.open(urls, (er, file) => {
-        let bf = new Buffer.from('112233')
-        console.log(er, 'er')
-        console.log(file, 'file')
-        fs.write(file, bf, 0, (err, resp) => {
-          console.log('err', err)
-          console.log('resp', resp)
-        })
-      })
-      console.log(e)
+    vscode.commands.registerCommand('xs-createApp', async function () {
+      const response = await vscode.window.showQuickPick(['vue后台', 'uni-app'], { placeHolder: '创建vuedemo' })
+      if (!response) return
+      const res = await vscode.window.showOpenDialog({ canSelectFolders: true, canSelectFiles: false, canSelectMany: false })
+      if (res.length) {
+        let url = res[0].path.replace('/', '')
+        copy(path.join(__dirname, 'template/vue'), url)
+        await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.parse(res[0], false))
+      }
     })
   )
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('showDodia', e => {
+    vscode.commands.registerCommand('showDialog', async e => {
       let files = fs.readdirSync(path.join(__dirname, 'xs-plugins/components'), { encoding: 'utf-8' })
       if (!files) return
       files = files.map(value => value.replace('.vue', ''))
-      vscode.window
-        .showQuickPick(files, {
-          placeHolder: '请选择组件'
+      const templateName = await vscode.window.showQuickPick(files, { placeHolder: '请选择组件' })
+      if (!templateName) return
+      const fileName = await vscode.window.showInputBox({ placeHolder: '输入组件名称', value: 'index' })
+      let urls = e.path.replace('/', '')
+      if (fileName.indexOf('.vue') !== -1) return vscode.window.showInformationMessage('不允许添加.vue后缀，请重试')
+      fs.readFile(path.join(__dirname, 'xs-plugins/components', `${templateName}.vue`), {}, function (err, data) {
+        fs.writeFile(`${urls}/${fileName}.vue`, data, function () {
+          vscode.window.showInformationMessage('添加成功')
         })
-        .then(res => {
-          if (!res) return
-          vscode.window
-            .showInputBox({
-              placeHolder: '输入组件名称',
-              value: 'index'
+      })
+    })
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('addFolder', async function (e) {
+      const fileName = await vscode.window.showInputBox({ placeHolder: '输入文件夹名称' })
+      if (!fileName) return
+      const urls = e.path.replace('/', '')
+      let files = fs.readdirSync(path.join(__dirname, 'xs-plugins/components'), { encoding: 'utf-8' })
+      if (!files) return
+      files = files.map(value => value.replace('.vue', ''))
+      const templateName = await vscode.window.showQuickPick(files, { placeHolder: '选择要创建的组件' })
+      fs.mkdir(path.join(urls, fileName), (err) => {
+        if (err == null && templateName) {
+          fs.readFile(path.join(__dirname, 'xs-plugins/components', `${templateName}.vue`), {}, function (err, data) {
+            fs.writeFile(`${urls}/${fileName}/${templateName}.vue`, data, writeErr => {
+              if (writeErr) return vscode.window.showErrorMessage(`创建失败：${JSON.stringify(writeErr)}  -writeFile`)
+              vscode.window.showInformationMessage('创建成功')
             })
-            .then(fileName => {
-              let urls = e.path.replace('/', '')
-              if (fileName.indexOf('.vue') !== -1) return vscode.window.showInformationMessage('不允许添加.vue后缀，请重试')
-              fs.readFile(path.join(__dirname, 'xs-plugins/components', `${res}.vue`), {}, function (err, data) {
-                fs.writeFile(`${urls}/${fileName}.vue`, data, function () {
-                  vscode.window.showInformationMessage('添加成功')
-                })
-              })
-            })
-        })
+          })
+        } else if (err) {
+          vscode.window.showErrorMessage(`创建失败：${JSON.stringify(err)}`)
+        }
+      })
     })
   )
 }
